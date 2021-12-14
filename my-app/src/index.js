05c3281893f48466be37233c24f5620546e4f14f
nuneva <radina.nuneva@vistajet.com>
2021-12-14 10:25:35 +0200

    Add initial react-redux project

---
+++ my-app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+import { Provider } from 'react-redux'; //connects store to App
+
+const store = createStore(rootReducer, 
+                            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+  <Provider store = {store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
